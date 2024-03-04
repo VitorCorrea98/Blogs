b58f5d3388295b553d50613bd0f89840f245d06a
Vitor Correa <vrcnode@gmail.com>
2024-03-04 17:01:24 -0300

    Requisito 1 feito

---
+++ src/migrations/20240304192841-blog_posts.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable("blog_posts", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      content: {
+        type: Sequelize.STRING
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+      },
+      published: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+
+  async down (queryInterface, _Sequelize) {
+    await queryInterface.dropTable("blog_posts");
+    
+  }
+};
